1fdf3c30a74d8a5cb642aee3d3bfab132a30bf2f
joelliusp <joelliuscaesar@gmail.com>
2017-10-12 16:35:06 -0400

    added a service

---
+++ SpaceService/CommonService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+
+namespace SpaceService
+{
+    public abstract class CommonService: Service1
+    {
+        public virtual bool ServiceIsRunning()
+        {
+            return true;
+        }
+
+        public virtual string[] ServiceInfo()
+        {
+            return new[] { String.Format("Name: {0}", OperationContext.Current.Host.Description.Name),
+                            String.Format("Endpoints: {0}",String.Join(",",OperationContext.Current.Host.Description.Endpoints.Select(e => e.ListenUri)))};
+        }
+
+
+    }
+}
